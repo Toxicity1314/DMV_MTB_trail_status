ea633b08085af2cc2064cec6f9a3f9e9463702ba
Sean Welsh <s.welsh1314@gmail.com>
2023-11-29 19:38:04 -0500

    updated home to look better

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,6 @@ import { AboutStyle } from "./styles";
 function About() {
   return (
     <AboutStyle>
-
       <div className="narrow">
         <p>
           Hello my name is Sean, and I am the creator of Virginia MTB Rain Data
@@ -42,61 +41,10 @@ function About() {
           Thank you for visiting my website, and I hope it helps you plan your
           next ride! Please feel free to send any feedback to
           toxicity1314@gmail.com and check out my{" "}
-          <a href="https://www.linkedin.com/in/sean-welsh">
-            LinkedIn
-          </a>
+          <a href="https://www.linkedin.com/in/sean-welsh">LinkedIn</a>
         </p>
       </div>
     </AboutStyle>
-    // <AboutStyle>
-
-    // <div className="about">
-    //   <h1>Welcome</h1>
-    //   <div className="content">
-    //     <div className="left">
-    //       <img
-    //               className="picture"
-    //               src={muddy}
-    //               alt="Mountain bike at the top of a mountain"
-    //               />
-    //               </div>
-    //               <div className="right">
-    //                 <p>
-    //                   Hello my name is Sean, and I am the creator of Virginia MTB Rain Data
-    //                   a website specifically designed for checking the amount of recent
-    //                   rainfall on a trail system. I developed this website because of my
-    //                   love for mountain biking and to showcase my skills as a recent
-    //                   graduate of the Flatiron School as a Software Developer.
-    //                 </p>
-    //                 <p>
-    //                   As a mountain biker, I know how frustrating it can be to get to
-    //                   a trail and find it's too muddy to ride. Rainfall can
-    //                   significantly impact the trail condition, making it either an
-    //                   enjoyable ride or an unpleasant experience that could damage the
-    //                   trails. With my website, I aim to provide accurate and
-    //                   up-to-date information on whether a trail is safe and rideable
-    //                   or too muddy and should be avoided.
-    //                 </p>
-    //               </div>
-    //             </div>
-    //             <p>
-    //               My coding skills acquired through the Flatiron School allowed me
-    //               to create a website that uses real-time weather data and
-    //               customized algorithms tailored to provide accurate rain data
-    //               calculations. My website is designed to help fellow mountain
-    //               bikers make informed decisions on whether to hit the trails or
-    //               wait for better weather conditions.
-    //             </p>
-    //             <p>
-    //               Thank you for visiting my website, and I hope it helps you plan your
-    //               next ride! Please feel free to send any feedback to
-    //               toxicity1314@gmail.com and check out my{" "}
-    //               <a target="_blank" href="https://www.linkedin.com/in/sean-welsh">
-    //                 LinkedIn
-    //               </a>
-    //             </p>
-    //           </div>
-    //               </AboutStyle>
   );
 }
 
---
+++ client/src/components/styles/HomeStyle.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export default styled.div`
+  .narrow {
+    padding: 0 21%;
+  }
+
+  .wrap {
+    float: left;
+    width: 100%;
+    margin-bottom: 20px;
+
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+  }
+
+  p {
+    margin: 0 0 15px 0;
+    text-align: justify;
+  }
+
+  h2 {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
+
+  .picture {
+    max-width: 100%;
+  }
+`;
